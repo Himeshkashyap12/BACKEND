09c82d4a55fc9785eee3cc93e44cd3736f8585f0
himesh.kashyap@celestialitverse.in <himeshkashyap347@hmail.com>
2025-05-21 14:45:33 +0530

    db connected

--- src/index.js
+++ src/index.js
@@ -1,24 +1,37 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constant.js";
-import express from "express";
-const app=express();
-(async()=>{
-     try {
-      const connectionInstance=await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-      console.log(connectionInstance);
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path:"./env"
+})
+
+connectDB();
+
+
+
+
+
+
+
+
+
+// (async()=>{
+//      try {
+//       const connectionInstance=await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//       console.log(connectionInstance);
       
-      app.on("error",(error)=>{
-        console.log("error",error);
-        throw error  ;
+//       app.on("error",(error)=>{
+//         console.log("error",error);
+//         throw error  ;
         
-      })
-      app.listen(process.env.PORT,()=>{
-      console.log(`App is listning on port ${process.env.PORT}`);
+//       })
+//       app.listen(process.env.PORT,()=>{
+//       console.log(`App is listning on port ${process.env.PORT}`);
       
-      })
-     } catch (error) {
-        console.error(error);
-        throw error;
+//       })
+//      } catch (error) {
+//         console.error(error);
+//         throw error;
         
-     }
-})()
+//      }
+// })()
